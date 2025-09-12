ac458933c53b09c985ba872be4b4e2148520120f
vinisouz4 <vini_wow16@hotmail.com>
2025-09-12 20:55:01 -0300

    Update e save em csv dos dados

--- src/routes/scraping.py
+++ src/routes/scraping.py
@@ -1,45 +1,52 @@
 from fastapi import APIRouter, HTTPException, Depends
 from pydantic import BaseModel
 
-from src.service.web_scraping import scrape_service
+from src.service.web_scraping import scrape_all_books, scrape_service_get_categories
 
 
 router_scraping = APIRouter(prefix="/scraping")
 
-class ScrapingRequest(BaseModel):
-
-    """Request model for web scraping.
-    Attributes:
-        url (str): The URL of the website to scrape.
-        tag (str): The HTML tag to search for.
-        class_name (str): The class name of the HTML elements to search for."""
-
+class ScrapingCategory(BaseModel):
     url: str
-    tag: str
-    class_name: str
-    pages: int = 1  # Número de páginas a serem raspadas, padrão é 1
 
 
-@router_scraping.post("/v1/extract")
-async def extract_data(request: ScrapingRequest):
+@router_scraping.post("/v1/update_books_bd")
+async def update_data(request: ScrapingCategory):
     """
-    Extract data from a website based on the specified tag and class name.
+    Extract data from a website based on the specified url and update the books database.
     Args:
-        request (ScrapingRequest): The request body containing the URL, tag, and class name.
+        request (ScrapingCategory): The request body containing the URL, tag, and class name.
     Returns:
-        dict: A dictionary containing the extracted data.
+        Message indicating the success of the operation.
     """
     
 
     try:
         
-        data = scrape_service(
-            request.url, 
-            request.tag, 
-            request.class_name,
-            request.pages
+        scrape_all_books(
+            request.url
+        )
+
+        
+        return {"Message": "Books tables updated successfully!"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+
+@router_scraping.post("/v1/update_categories")
+async def update_categories(request: ScrapingCategory):
+    """
+    Extract categories from a website based on the specified tag and class name.
+    Args:
+        request (ScrapingRequest): The request body containing the URL, tag, and class name.
+    Returns:
+        dict: A dictionary containing the extracted categories.
+    """
+    try:
+        scrape_service_get_categories(
+            request.url
         )
         
-        return {"data": data}
+        return {"message": "Categories extracted and saved successfully!"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
